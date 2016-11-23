bee0cfa5864793934c8bf87ce4d8907ba1bd5f92
syhily <syhily@gmail.com>
2016-11-23 11:57:37 +0800

    Initial Commit

---
+++ src/main/java/me/yufan/gossip/Gossip.java
@@ -0,0 +1,10 @@
+package me.yufan.gossip;
+
+/**
+ * Hello world!
+ */
+public class Gossip {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/me/yufan/gossip/GossipAppTest.java
@@ -0,0 +1,33 @@
+package me.yufan.gossip;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class GossipAppTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public GossipAppTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(GossipAppTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
